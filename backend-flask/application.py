9eddf3a55a2a4538e5b5f15d340f13dd073ded4a
SrKoDes <desravines.kohiin@gmail.com>
2021-12-18 16:42:50 -0500

    Please enter the commit message for your changes. Lines starting deleted venv

--- backend-flask/application.py
+++ backend-flask/application.py
@@ -1,7 +1,8 @@
 from subprocess import list2cmdline
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, jsonify
 import handler
 import json
+import requests
 
 application = Flask(__name__)
 
@@ -16,7 +17,7 @@ session_username = ""
 # Pulls username
 @application.route('/login')
 def get_username():
-    
+
     request_data = request.get_json()
     print(request_data)
     # session_username = 
@@ -30,6 +31,31 @@ def display_dashboard():
     user_info = handler.get_user_info(f'https://api.github.com/users/{session_username}')
 
     return list_of_repos, user_info
+
+
+@application.route('/getGithubToken')
+def exchange_token():
+    exchange_code = str(request.args.get('code'))
+    
+    # json_fcm_data = {"client_id": '0923bbef1520f84ac3e1', "client_secret": '9e75000e02defa3b5e7abe31ccb4fc16860370b7', "code": exchange_code}
+    
+    # json_string = json.dumps(json_fcm_data)
+
+    # headers = {'Accept': 'application/json'}
+
+    # r = requests.post("https://github.com/login/oauth/access_token", headers=headers, data=json_fcm_data)
+
+    # data = jsonify(r)
+    # rr = Response(json.dumps(data), mimetype = 'application/json')
+    
+    github_token_exhange = requests.post(f"https://github.com/login/oauth/access_token?code={exchange_code}?client_id=0923bbef1520f84ac3e1?client_secret=9e75000e02defa3b5e7abe31ccb4fc16860370b7")
+    # json_token = github_token_exhange[0]
+    response = Response(json.dumps(github_token_exhange), status = '200', mimetype = 'application/json')
+    # # print("hello")
+
+    # return rr
+    return render_template('home.html', r=response)
+
 
 # Deploys the customer's app
 @application.route('/start_deployment')
