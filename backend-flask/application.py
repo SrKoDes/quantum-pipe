30c61999def0c76ac93bfbf735b25cfc6ae551aa
SrKoDes <desravines.kohiin@gmail.com>
2021-12-18 13:10:38 -0500

    sdf

--- backend-flask/application.py
+++ backend-flask/application.py
@@ -1,3 +1,4 @@
+from subprocess import list2cmdline
 from flask import Flask, render_template, request, Response
 import handler
 import json
@@ -13,15 +14,25 @@ session_username = ""
 
 
 # Pulls username
-@application.route('/signin')
+@application.route('/login')
 def get_username():
+    
     request_data = request.get_json()
     print(request_data)
     # session_username = 
     return session_username
 
-#displays dashboard using username
+# Displays dashboard by pulling data from GitHub API using session_username.
 @application.route('/dashboard')
 def display_dashboard():
-    # test
-    'https://api.github.com/users/hector6921/repos'
+
+    list_of_repos = handler.get_repos(f'https://api.github.com/users/{session_username}/repos')
+    user_info = handler.get_user_info(f'https://api.github.com/users/{session_username}')
+
+    return list_of_repos, user_info
+
+# Deploys the customer's app
+@application.route('/start_deployment')
+def deploy_app():
+
+    handler.build_app()
