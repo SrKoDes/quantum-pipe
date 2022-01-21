e341144d22f9aa34e5cb18019e42b5ff564c63c3
hector6921 <hector_6921@hotmail.com>
2022-01-21 12:19:07 -0500

    wws

--- backend-flask/terraform/outputs.tf
+++ backend-flask/terraform/outputs.tf
@@ -1,4 +1,9 @@
 output "instance_pub_ip" {
   description = "Public IP of the EC2"
-  value       = aws_instance.EC2.public_ip
+  value       = aws_instance.EC2-main.public_ip
+}
+
+output "terraform-instance_pub_ip" {
+  description = "Public IP of the EC2"
+  value       = aws_instance.EC2-terraform.public_ip
 }
