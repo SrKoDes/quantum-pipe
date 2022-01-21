aa7bd416453a3697201d0ac79b7bdd74f1e75183
hector6921 <hector_6921@hotmail.com>
2022-01-21 12:23:46 -0500

    jee5

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,10 @@
 output "instance_pub_ip" {
-  description = "Private IP of the EC2"
-  value       = aws_instance.EC2.public_ip
+  description = "Public IP of the EC2"
+  value       = aws_instance.EC2-main.public_ip
+}
+
+
+output "terraform-instance_pub_ip" {
+  description = "Public IP of the EC2"
+  value       = aws_instance.EC2-terraform.public_ip
 }
