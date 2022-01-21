2ab351b5741cd1fa277f5420dfcac104b7bd6227
SrKoDes <desravines.kohiin@gmail.com>
2022-01-21 14:19:45 -0500

    update

--- backend-flask/terraform/provision.sh
+++ backend-flask/terraform/provision.sh
@@ -2,7 +2,7 @@
 
 terraform init
 terraform apply --auto-approve
-
+chmod 400 user_app_key
 # need to ssh-keygen -f "filepath/filename" to create pub and prviate. create ec2 with pub, use private to ssh"
 
 # ssh -i terraform-keys -o "StrictHostKeyChecking no" ubuntu@$(terraform output -raw instance_pub_ip)
