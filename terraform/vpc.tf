2fbf3ba0c295f9c84815f6f9e82b41e30a3dadbf
SrKoDes <desravines.kohiin@gmail.com>
2022-01-20 21:50:43 -0500

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@
 # CREATE EC2    
 resource "aws_instance" "EC2" {
   ami                    = "ami-01f096662a5ade245"
-  instance_type          = "m4.medium"
+  instance_type          = "m5.large"
   key_name               = "app_key"
   vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.flask.id, aws_security_group.react.id]
 
