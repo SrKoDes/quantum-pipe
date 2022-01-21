3e81ed1a1a608ff56a8c45120b077b6109e519e1
SrKoDes <desravines.kohiin@gmail.com>
2022-01-21 18:15:12 -0500

    update

--- backend-flask/terraform/provision.sh
+++ backend-flask/terraform/provision.sh
@@ -8,6 +8,6 @@ chmod 400 user_app_key
 # need to ssh-keygen -f "filepath/filename" to create pub and prviate. create ec2 with pub, use private to ssh"
 
 # ssh -i terraform-keys -o "StrictHostKeyChecking no" ubuntu@$(terraform output -raw instance_pub_ip)
-$(terraform output -raw instance_pub_ip) > user_app_ip
+$(terraform output -raw instance_pub_ip) > ../user_app_ip
 scp -i user_app_key -o "StrictHostKeyChecking no" Dockerfile ubuntu@$(terraform output -raw instance_pub_ip):/home/ubuntu
 ssh -i user_app_key ubuntu@$(terraform output -raw instance_pub_ip) 'bash -s' < build.sh $1 $2
